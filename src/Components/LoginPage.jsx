d31b7e3e2957146742415296a2495be0ec1c78d5
ali-skinner <ali.skinner.15@gmail.com>
2025-02-05 12:45:44 -0600

    notes for login screen

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
- function LoginPage() {
+function LoginPage() {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [username, setUsername] = useState();
     const [register, setRegister] = useState();
-
+// will I need useEffect? to make sure this login components shows on mount only, can't keep loggin in if already active/login = true
     return (
         <>
             <form>
@@ -17,13 +17,23 @@ import React, { useState } from "react";
                     </div>
                     <div className="login-Input">
                         <label htmlFor="password">Password</label>
-                        <input value={password} onChange={(e) => setPassword(e.target.value)} type="text" placeholder="password" name="password" id="password" />
+                        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" name="password" id="password" />
+
+                    </div>
+                    <div className="button-container">
+                        {/* Log In onClick - add username, email, password to userprofile object/ set login to true/ load Search screen/component */}
                         <button className="button-login">Log In</button>
+
+                        {/* Register onClick - Shows Register screen inputs/ Hides login In Screen inputs */}
+                        <button className="button-login">Register</button>
                     </div>
                     <div className="login-Input">
                         <label htmlFor="register">Register</label>
                         <input value={register} onChange={(e) => setRegister(e.target.value)} type="text" placeholder="register" name="register" id="register" />
+                    </div>
+                    <div>
                         <button className="button-login">Create Username</button>
+                        {/* onClick - add username, email, password to userprofile object/ set login to true/ load Search screen/component */}
                     </div>
                 </div>
             </form>
