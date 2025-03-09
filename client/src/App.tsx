1ce2ee5bd97f1cb467c1af31cfc0720f385ae0da
ali-skinner <ali.skinner.15@gmail.com>
2025-03-09 14:55:26 -0500

    drafting favorites functions - need to explore toggle and extracting as components - nav next

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,62 +7,43 @@ import LoginPage from './components/LoginPage';
 import FavoritesPage from './components/FavoritesPage';
 
 
+const UserContext = createContext(null);
+const useUser = useContext(UserContext);
 
 
 function App() {
-const [user, setUser] = useState('');
-const [favorites, setFavorites] = useState([]);
-//const addToFavorites function
-//const removeFromFavorites function
-//are these functions a toggle? would be nice to remove fav on search page if accidentally click fav button
-//do i need to save to local storage?
-//can i move these to components? how does this move affect useState? need to call addtoFavs in SearchComp. 
+  const [user, setUser] = useState('');
+  const [favorites, setFavorites] = useState([]);
+
+  //are these FAV functions toggling? would be nice to remove fav on search page if accidentally click fav button
+  //do i need to save to local storage?
+  //can i move these to components? how does this move affect useState? need to call addtoFavs in SearchComp. 
+
+  const addToFavorites = (gif) => {
+    setFavorites(prevFavorites => {
+      if (!prevFavorites.some(fav => fav.id === gif.id)) {
+        return [...prevFavorites, gif];
+      }
+      return prevFavorites;
+    });
+  };
+
+  const removeFromFavorites = (gifId) => {
+    setFavorites(prevFavorites =>
+      prevFavorites.filter(gif => gif.id !== gifId)
+    );
+  }; //why dont i have to return preFavs? and not using the spreader operator to keep track of whts removed? why doesnt removeFavs match addFavs return stmnt?
 
 
   return (
     <>
-      {/* <LoginPage /> */}
-      <SearchPage />
-      {/* <FavoritesPage /> */}
+      <UserContext.Provider value={{ user, setUser, favorites, addToFavorites, removeFromFavorites }}>
+        <Router>
+
+        </Router>
+      </UserContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
-
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
+export default App;
